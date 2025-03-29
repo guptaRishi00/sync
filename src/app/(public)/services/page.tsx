2c17a1d7336bdad2b5dac2ab6a4fb72b303cf7d9
Balaji <d.balaji.mc@gmail.com>
2025-03-29 12:18:12 +0530

    Finishing Services laoyout

--- src/app/(public)/services/page.tsx
+++ src/app/(public)/services/page.tsx
@@ -1,5 +1,4 @@
 import BookAppointmentButton from "@/components/prefabs/book-appointment-button";
-import CommitmentToPatientSection from "@/components/prefabs/commitement-to-patient-section";
 import ConnectToSyncSection from "@/components/prefabs/connect-to-sync-section";
 import DecorImage from "@/components/prefabs/decor-image";
 import Footer from "@/components/prefabs/footer";
@@ -25,19 +24,11 @@ export default async function ServicesPage() {
             </main>
 
             <main className="main flex flex-col">
-                <ConsultationSection1 />
+                <PsychiatricConsultationsSection />
             </main>
 
             <main className="main bg-secondary/20 flex flex-col">
-                <ConsultationSection />
-            </main>
-
-            <main className="main flex flex-col">
-                <ConsultationSection1 />
-            </main>
-
-            <main className="main md:min-h-fit!">
-                <CommitmentToPatientSection />
+                <CounsellingAndPsychotherapy />
             </main>
 
             <main className="main overflow-hidden py-8">
@@ -78,7 +69,7 @@ function HeroSection() {
 
             <div className="md:grow"></div>
 
-            <div className="grid grid-cols-1 grid-rows-2 gap-6 md:grid-cols-2 md:grid-rows-1">
+            <div className="grid grow grid-cols-1 grid-rows-2 gap-6 md:grid-cols-2 md:grid-rows-1">
                 <div className="flex grow flex-col gap-6">
                     <div className="">
                         <h2 className="relative inline text-3xl font-bold md:text-5xl">
@@ -99,9 +90,9 @@ function HeroSection() {
                     <div className="bg-primary relative grow rounded-2xl p-8">
                         <h2 className="text-xl font-bold md:text-3xl">Mission</h2>
                         <p className="z-50 md:pb-34">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-                            a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                            industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book.
                         </p>
 
                         <DecorImage
@@ -209,7 +200,7 @@ function EvaluationMethodologiesSection() {
                             ))}
                         </ul>
 
-                        <div className="border-primary absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-2xl border-3 bg-white p-3">
+                        <div className="border-primary absolute bottom-0 left-1/2 w-38 -translate-x-1/2 translate-y-1/2 rounded-2xl border-3 bg-white p-3 text-center">
                             {item.title}
                         </div>
 
@@ -217,7 +208,21 @@ function EvaluationMethodologiesSection() {
                             src="/images/decor-plant-1.png"
                             alt="Decor Highlight"
                             size={[80, 80]}
-                            className="right-0 bottom-0 z-10 translate-y-1/6"
+                            className="right-0 bottom-0 z-40 translate-y-1/6"
+                        />
+
+                        <DecorImage
+                            src="/images/decor-butterfly-2.png"
+                            alt="Decor Smile"
+                            size={[100, 100]}
+                            className="border-primary top-1/2 right-0 z-20 translate-x-1/2 -translate-y-1/2 rounded-2xl border-3 bg-white p-4"
+                        />
+
+                        <DecorImage
+                            src="/images/decor-highlight.png"
+                            alt="Decor Highlight"
+                            size={[16, 16]}
+                            className="bottom left-0 z-20 -translate-x-full translate-y-full -rotate-90"
                         />
                     </TabsContent>
                 ))}
@@ -233,7 +238,7 @@ function ConsultationSection() {
                 <span className="bg-primary rounded-sm px-1">Consultation</span>
             </h2>
 
-            <div className="grid w-full grid-cols-1 md:grid-cols-[40fr_60fr] md:gap-12">
+            <div className="relative mb-10 grid w-full grid-cols-1 md:grid-cols-[40fr_60fr] md:gap-12">
                 <div className="relative mr-8 mb-16 aspect-49/50 w-full">
                     <Image
                         src="/images/services-bg-1.jpg"
@@ -247,7 +252,7 @@ function ConsultationSection() {
                         src="/images/decor-matrix.png"
                         alt="Decor Butterfly"
                         size={[160, 160]}
-                        className="right-0 bottom-0 z-10 -translate-x-1/8 translate-y-1/2 stroke-3"
+                        className="right-0 bottom-0 z-10 -translate-x-1/8 translate-y-1/4 stroke-3 md:translate-y-1/2"
                     />
                 </div>
 
@@ -270,12 +275,12 @@ function ConsultationSection() {
                     </h2>
                     <div className="flex">
                         <p className="inline text-justify">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-                            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-                            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-                            of Lorem Ipsum.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                            industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+                            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                            PageMaker including versions of Lorem Ipsum.
                         </p>
                         <div className="relative ml-6 h-full w-12 md:w-5">
                             <DecorImage
@@ -286,22 +291,120 @@ function ConsultationSection() {
                             />
                         </div>
                     </div>
-                    <BookAppointmentButton />
+
+                    <div className="relative">
+                        <DecorImage
+                            src="/images/decor-arrow.png"
+                            alt="Decor Butterfly"
+                            size={[60, 60]}
+                            className="right-0 bottom-0 z-10 translate-x-[120%] stroke-3"
+                        />
+                        <BookAppointmentButton />
+                    </div>
                 </div>
+
+                <DecorImage src="/images/decor-butterfly.png" alt="Decor Butterfly" size={[44, 44]} className="top-0 right-0" />
             </div>
+
+            <DecorImage
+                src="/images/decor-leaves-1.png"
+                alt="Decor Butterfly"
+                size={[180, 180]}
+                className="invisible bottom-0 left-0 z-10 translate-y-1/2 md:visible"
+            />
         </section>
     );
 }
 
-function ConsultationSection1() {
+function PsychiatricConsultationsSection() {
     return (
         <section className="section relative flex h-full grow flex-col items-center justify-center gap-6 py-8 md:gap-12">
             <h2 className="relative text-3xl font-bold md:mb-4 md:text-5xl">
-                <span className="bg-primary rounded-sm px-1">Consultation</span>
+                Psychiatric <span className="bg-primary rounded-sm px-1">Consultation</span>
             </h2>
 
-            <div className="grid w-full grid-cols-1 md:grid-cols-[60fr_40fr] md:gap-12">
-                <div className="relative order-last mr-8 mb-16 aspect-49/50 w-full">
+            <div className="mb-10 grid w-full grid-cols-1 md:grid-cols-[60fr_40fr] md:gap-12">
+                <div className="relative flex flex-col items-start justify-center gap-8">
+                    <h2 className="relative text-3xl tracking-normal break-normal md:text-5xl">
+                        Child Psychiatry
+                        <Image
+                            src="/images/decor-highlight-2.png"
+                            alt="Decor Highlight"
+                            fill
+                            sizes="80"
+                            className="bottom-0 left-0 -z-10 translate-y-2/3 object-contain"
+                        />
+                        <DecorImage
+                            src="/images/decor-smile-primary.png"
+                            alt="Decor Smile"
+                            size={[58, 58]}
+                            className="top-0 left-0 -translate-y-full"
+                        />
+                    </h2>
+                    <div className="flex">
+                        <div className="relative mr-6 h-full w-12 md:w-5">
+                            <DecorImage
+                                src="/images/decor-highlight-3.png"
+                                alt="Decor Butterfly"
+                                size={[500, 500]}
+                                className="top-0 left-0"
+                            />
+                        </div>
+                        <p className="inline text-justify">
+                            Child and adolescent psychiatry is a branch of medicine that focuses on the diagnosis, treatment, and prevention
+                            of mental health disorders in children and adolescents. Psychiatrists who specialize in this field are trained
+                            to work with young people, ranging from infancy through adolescence, who are experiencing emotional, behavioral,
+                            or developmental problems.
+                        </p>
+                    </div>
+
+                    <div className="relative">
+                        <DecorImage
+                            src="/images/decor-arrow.png"
+                            alt="Decor Butterfly"
+                            size={[60, 60]}
+                            className="right-0 bottom-0 z-10 translate-x-[120%] stroke-3"
+                        />
+                        <BookAppointmentButton />
+                    </div>
+                </div>
+                <div className="relative order-first mr-8 mb-16 aspect-49/50 w-full md:order-last">
+                    <Image
+                        src="/images/services-bg-2.jpg"
+                        fill
+                        sizes="100vw"
+                        alt="Service BG"
+                        className="z-20 h-full w-full overflow-hidden rounded-tl-[120px] rounded-br-[120px] object-cover"
+                    />
+
+                    <DecorImage
+                        src="/images/decor-matrix.png"
+                        alt="Decor Butterfly"
+                        size={[160, 160]}
+                        className="right-0 bottom-0 z-10 -translate-x-1/8 translate-y-1/4 stroke-3 md:left-0 md:translate-x-1/8 md:translate-y-1/2"
+                    />
+                </div>
+            </div>
+
+            <DecorImage
+                src="/images/decor-leaves-1.png"
+                alt="Decor Butterfly"
+                size={[180, 180]}
+                className="invisible right-0 bottom-0 z-10 translate-y-1/2 md:visible"
+            />
+        </section>
+    );
+}
+
+function CounsellingAndPsychotherapy() {
+    return (
+        <section className="section relative flex h-full grow flex-col items-center justify-center gap-6 py-8 md:gap-12">
+            <h2 className="relative text-3xl font-bold md:mb-4 md:text-5xl">
+                <span className="bg-primary rounded-sm px-1">Counselling</span> and Psychotherapy
+            </h2>
+
+            <div className="relative mb-10 grid w-full grid-cols-1 md:grid-cols-[40fr_60fr] md:gap-12">
+                <div className="relative mr-8 mb-16 aspect-49/50 w-full">
                     <Image
                         src="/images/services-bg-1.jpg"
                         fill
@@ -314,13 +417,13 @@ function ConsultationSection1() {
                         src="/images/decor-matrix.png"
                         alt="Decor Butterfly"
                         size={[160, 160]}
-                        className="right-0 bottom-0 z-10 -translate-x-1/8 translate-y-1/2 stroke-3"
+                        className="right-0 bottom-0 z-10 -translate-x-1/8 translate-y-1/4 stroke-3 md:translate-y-1/2"
                     />
                 </div>
 
                 <div className="relative flex flex-col items-start justify-center gap-8">
                     <h2 className="relative text-3xl tracking-normal break-normal md:text-5xl">
-                        Consulation
+                        Individual Psychotherapy
                         <Image
                             src="/images/decor-highlight-2.png"
                             alt="Decor Highlight"
@@ -337,12 +440,10 @@ function ConsultationSection1() {
                     </h2>
                     <div className="flex">
                         <p className="inline text-justify">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-                            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-                            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-                            of Lorem Ipsum.
+                            A 60-minute session where psychologists take a brief history of your background and identify what struggles you
+                            have been going through. By doing so, the psychologist will be able to decide which assessment to administer and
+                            which approach to take for treatment. This service is best for people who are starting their mental health
+                            journey for the first time at Sync positive.
                         </p>
                         <div className="relative ml-6 h-full w-12 md:w-5">
                             <DecorImage
@@ -353,9 +454,27 @@ function ConsultationSection1() {
                             />
                         </div>
                     </div>
-                    <BookAppointmentButton />
+
+                    <div className="relative">
+                        <DecorImage
+                            src="/images/decor-arrow.png"
+                            alt="Decor Butterfly"
+                            size={[60, 60]}
+                            className="right-0 bottom-0 z-10 translate-x-[120%] stroke-3"
+                        />
+                        <BookAppointmentButton />
+                    </div>
                 </div>
+
+                <DecorImage src="/images/decor-butterfly.png" alt="Decor Butterfly" size={[44, 44]} className="top-0 right-0" />
             </div>
+
+            <DecorImage
+                src="/images/decor-leaves-1.png"
+                alt="Decor Butterfly"
+                size={[180, 180]}
+                className="invisible right-0 bottom-0 z-10 translate-y-1/2 md:visible"
+            />
         </section>
     );
 }
