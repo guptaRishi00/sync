171af044a49cd507679312c6dd80f426d21e5980
guptaRishi00 <rishigupta54066@gmail.com>
2025-06-25 12:44:09 +0530

    added google anlytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import FloatingContent from "@/components/prefabs/floating-content";
 import { frameMetadata } from "@/lib/utils";
 import GlobalProvider from "@/providers/global-provider";
 import type { Metadata } from "next";
-import "./globals.css"; // Global styles for the entire app
+import "./globals.css";
 import { getHomePageData } from "@/data/loader";
+import Script from "next/script"; // Add this import
 
 export const metadata: Metadata = frameMetadata("Home");
 
@@ -18,6 +19,24 @@ export default async function RootLayout({ children }: Props) {
         <html lang="en" suppressHydrationWarning>
             <head>
                 <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+                {/* Add Google Analytics Scripts Here */}
+                <Script
+                    id="google-analytics-external"
+                    strategy="afterInteractive"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-M3VKCX5SJR"
+                />
+                <Script
+                    id="google-analytics-inline"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-M3VKCX5SJR');
+                        `,
+                    }}
+                />
             </head>
             <body>
                 <FloatingContent bookAppointmentButton={bookAppointmentButton} />
