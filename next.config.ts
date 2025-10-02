b2d984d639acd74de397505e0bdeef70cdb0ade9
guptaRishi00 <rishigupta54066@gmail.com>
2025-10-02 16:55:28 +0530

    feat: updated next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ["127.0.0.1", "localhost", "loving-peace-bbcfb23bbf.media.strapiapp.com", "loving-peace-bbcfb23bbf.strapiapp.com"],
+        domains: [
+            "127.0.0.1",
+            "localhost",
+            "loving-peace-bbcfb23bbf.media.strapiapp.com",
+            "loving-peace-bbcfb23bbf.strapiapp.com",
+            "celebrated-wealth-76f0bbd82e.strapiapp.com",
+            "celebrated-wealth-76f0bbd82e.media.strapiapp.com",
+        ],
     },
 };
 
